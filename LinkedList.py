d54efb423f9fc0dd7e82e0e03a3ec2412cffcbd2
Shivang Dave <daveshivang15@gmail.com>
2019-02-20 21:59:50 -0600

    Remove Test Code from Source

--- LinkedList.py
+++ LinkedList.py
@@ -28,16 +28,3 @@ class LinkedList:
             previousNode = currentNode
             currentNode = currentNode.nextNode
         return head
-
-# node1 = Node("3")
-# node2 = Node("7")
-# node3 = Node("10")
-#
-# node1.nextNode = node2
-# node2.nextNode = node3
-# head = node1
-# currentNode = head
-#
-# while currentNode is not None:
-#     print(currentNode.value + "->")
-#     currentNode = currentNode.nextNode
